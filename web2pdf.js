d52dd9f09fef5de90b7b5d967f62d05b9c68efb8
futuremaze <future.maze@gmail.com>
2018-05-17 17:49:54 +0900

    サーバ機能を追加

--- web2pdf.js
+++ web2pdf.js
@@ -1,71 +1,116 @@
 var prog = process.argv[1].replace(/^.*[\\\/]/, '');
 
-function check_args(args) {
-  if(args.targets.length != 1) {
-    console.error(`${prog}:Fatal:Url is not given.`);
-    argv.help();
-    process.exit(1);
-  }
-  args.options.outputdir = args.options.outputdir || "./";
-  args.options.device = args.options.device || "iPhone 6";
-  // if(devices.indexOf(args.options.device) == -1) {
-  //   console.error(`${prog}:Fatal:The specified device does not exist.`);
-  //   console.error(`  You can select from the devices described below.`);
-  //   for (var i=0; i<devices.length; i++) {
-  //     console.error(`    '${devices[i].name}'`);
-  //   }
-  //   process.exit(1);
-  // }
-
-  return(args);
-}
-
-function do_web2pdf(args) {
-  args = check_args(args);
+function do_web2pdf(url, options={}, callback=undefined) {
+  if (url === undefined) return(1);
+  var emulate_device = options.device || "iPhone 6";
+  var output_dir = options.outputdir || "./";
+  var callback_on_complete = callback || function(result_code){};
 
   (async () => {
     const fs = require('fs-extra');
     const puppeteer = require('puppeteer');
     const devices = require('puppeteer/DeviceDescriptors');
     const browser = await puppeteer.launch({
-      // headless: false,
       "ignoreHTTPSErrors": true
     });
     const page = await browser.newPage();
 
     try {
-      await page.emulate(devices[args.options.device]);
-      await page.goto(args.targets[0], {waitUntil: "domcontentloaded"});
+      await page.emulate(devices[emulate_device]);
+      await page.goto(url, {waitUntil: "domcontentloaded"});
     } catch (e) {
       console.error(`${prog}:${e}`);
-      process.exit(1);
-    }
-  
-    // output pdf.
+      callback_on_complete(1);
+    }  
+
+    // convert to pdf.
     var filename = (await page.title() + '.pdf').replace('/','／');
-    var width = await devices[args.options.device].viewport.width;
-    var height = await devices[args.options.device].viewport.height;
-    var deviceScaleFactor = await devices[args.options.device].viewport.deviceScaleFactor;
+    var width = await devices[emulate_device].viewport.width;
+    var height = await devices[emulate_device].viewport.height;
+    var deviceScaleFactor = await devices[emulate_device].viewport.deviceScaleFactor;
 
     try {
-      fs.mkdirsSync(args.options.outputdir);
+      fs.mkdirsSync(output_dir);
     } catch (e) {
       console.error(`${prog}:${e}`);
-      process.exit(1);
+      callback_on_complete(1);
     }
+
     await page.pdf({
-      path: args.options.outputdir + '/' + filename,
+      path: `${output_dir}/${filename}`,
       printBackground: true,
       width: width * deviceScaleFactor,
       height: height * deviceScaleFactor
     });
+
     await browser.close();
+    callback_on_complete(0);
   })();
+}
+
+function launch_server(options={}) {
+  var http   = require('http');
+  var url    = require('url');
+  var server = http.createServer();
+  var port   = options.port || 8080;
+
+  server.on('request', function(req, res) {
+    var url_path = url.parse(req.url).pathname;
+    var body = '';
+
+    if (url_path == '/web2pdf') {
+      if (req.method == 'POST' &&
+        req.headers['content-type'] == 'application/json') {
+        req.on('data', function(data) { body += data; });
+        req.on('end', function(){
+          try {
+            var json = JSON.parse(body);
+          } catch(e) {
+            console.error(`${prog}:${e}`);
+            return;
+          }
+          
+          var url = json.url;
+          options.device = json.device;
+          options.port = port;
+          do_web2pdf(url, options, function(result_code) {
+            if (result_code == 0) {
+              res.writeHead(200, {'Content-Type': 'text/plain'});
+              res.write('OK\n\n');
+            } else {
+              res.writeHead(500);
+            }
+            res.end();
+          });
+        });
+      }
+    }
+  });
+
+  try {
+    server.listen(port);
+  } catch(e) {
+    console.error(`${prog}:${e}`);
+    process.exit(1);
+  }
+}
+
+function check_args(args) {
+  args.options.outputdir = args.options.outputdir || "./";
+  args.options.device = args.options.device || "iPhone 6";
+  args.options.server = args.options.server || false;
+  args.options.port = args.options.port || 8080;
+
+  if(!args.options.server && args.targets.length != 1) {
+    console.error(`${prog}:Fatal:Url is not given.`);
+    process.exit(1);
+  }
+ 
+  return(args);
 }
 
 function do_main() {
   var argv = require('argv');
-  var devices = require('puppeteer/DeviceDescriptors');
   
   argv.version('v1.0');
   argv.info(`${prog} [options] url`);
@@ -83,9 +128,29 @@ function do_main() {
       description: 'Emulate device to access url.',
       example: `'${prog} -d "iPhone 6" url' or '${prog} --device "iPhone 6" url'`
   });
+  argv.option({
+      name: 'server',
+      short: 's',
+      type: 'boolean',
+      description: 'Start up in server mode.',
+      example: `'${prog} -s url' or '${prog} --server url'`
+  });
+  argv.option({
+      name: 'port',
+      short: 'p',
+      type: 'int',
+      description: 'Set the port of the server.(valid in server mode.)',
+      example: `'${prog} -s -p 8080 url' or '${prog} --server --port 8080 url'`
+  });
 
   var args = argv.run();
-  do_web2pdf(args);
+  if (args.options.server) {
+    launch_server(args.options);
+  } else {
+    do_web2pdf(args.targets[0], args.options, function(result_code) {
+      console.log(`Complete(${result_code})`);
+    });
+  }
 }
 
 do_main();
